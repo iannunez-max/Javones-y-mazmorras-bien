5fedaca456c7b74e6db52345db4abf19a4b57c5b
iannunez-max <iannumi24@alumnes.ilerna.com>
2025-03-24 17:56:47 +0100

    Update Personaje.java

--- Personaje.java
+++ Personaje.java
@@ -1,5 +1,25 @@
 package javones_y_mazmorras;
 
-public class Personaje {
+class Personaje {
+String nombre;
+  int vida:
+Inventario inventario;
 
+  public Personaje(String nombrt, int vida){
+    this.nombre = nombre;
+    this.vida=vida;
+    this.inventario = new inventario();
+  }
+  public void bajarVida(int cantidad) {
+  if(cantidad>0) {
+ vida-= cantidad ;
+    if (vida <0){
+      vida = 0;
+    }
+  }
+  }
+  public String getNombre() {
+    return nombre;
+  }
 }
+

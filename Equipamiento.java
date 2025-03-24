ba0b1b12aacbf45ca9340ada1ee85c2a8ef45685
CocaAleix <aleixgoungra@alumnes.ilerna.com>
2025-03-24 19:53:24 +0100

    Update Equipamiento.java

--- Equipamiento.java
+++ Equipamiento.java
@@ -1,5 +1,19 @@
 package javones_y_mazmorras;
 
 public class Equipamiento {
+  String nombre;
+  String tipo;
 
+  public Equipamiento(String nombre, String tipo) {
+    this.nombre = nombre;
+    this.tipo = tipo;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public String getTipo() {
+    return tipo;
+  }
 }

cc19b0f0548cbbb1d79aae02652da9cb5e4b779c
iannunez-max <iannumi24@alumnes.ilerna.com>
2025-03-24 19:26:28 +0100

    Update Inventario.java

--- Inventario.java
+++ Inventario.java
@@ -1,5 +1,24 @@
 package javones_y_mazmorras;
 
-public class Inventario {
+import java.util.ArrayList;
 
+class Inventario {
+
+  ArrayList<Equipamiento>equipamiento;
+
+  public Inventario() {
+    equipamiento = new ArrayList<>();
+  }
+  public void agregarEquipamiento(Equipamiento e){
+    equipamiento add (e);
+  }
+  public void mostrarInventario() {
+    Sstem.out.println("Inventario");
+    int i =0;
+    while(i<equipamiento.size()) {
+      Equipamiento item=equipamiento.get(i);
+      System.out.println(item.getNombre() + "-" + item.getTipo());
+      i++;
+    }
+  }
 }

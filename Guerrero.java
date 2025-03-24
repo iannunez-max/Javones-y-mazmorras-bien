900bf5927f53c2e12abd64dcc8ef01193509ed7f
iannunez-max <iannumi24@alumnes.ilerna.com>
2025-03-24 18:53:15 +0100

    Update Guerrero.java

--- Guerrero.java
+++ Guerrero.java
@@ -1,5 +1,23 @@
 package javones_y_mazmorras;
 
-public class Guerrero {
+class Guerrero extends Personaje{
+Personaje companeroProtegido;
 
+  public Guerrero(String nombre) {
+    super (nombre,100);
+  }
+  public void proteger(Personaje companero){
+    if(companeroProtegido == null){
+      companeroProtegido= companero;
+      System.out.println(nombre + "esta protegiendo a" + companero.getNombre());
+    }else{
+      System.out.println(nombre + "ya esta protegiendo a" + companeroProtegido.getNombre());
+    }
+  }
+  public void dejarDeProteger() {
+    if(companeroProtegido != null) {
+      System.out.println(nombre+ "dejo de proteger a " + companeroProtegido.getNombre());
+      companeroProtegido =null;
+    }
+  }
 }

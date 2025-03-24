174530aa0992c78270695f5c11836fb85e0f8a0b
CocaAleix <aleixgoungra@alumnes.ilerna.com>
2025-03-24 19:33:31 +0100

    Update Ladron.java

--- Ladron.java
+++ Ladron.java
@@ -1,5 +1,34 @@
 package javones_y_mazmorras;
 
-public class Ladron {
+public class Ladron extends Personaje {
+  boolean invisible;
 
+  public Ladron(String nombre) {
+    super(nombre, 80);
+    invisible = false;
+  }
+
+  public void robar() {
+    if (invisible) {
+      System.out.println(nombre + " roba en las sombras sin ser visto.");
+    } else {
+      System.out.println(nombre + " roba, pero lo pueden ver.");
+    }
+  }
+  public void hacerseInvisible() {
+    invisible = !invisible;
+    if (invisible) {
+      System.out.println(nombre + " ahora es invisible");
+    } else {
+      System.out.println(nombre + " ahora es visible");
+    }
+  }
+}
+   
+
+
+
+
+
+  
 }
